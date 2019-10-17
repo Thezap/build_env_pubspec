849895e8a498bf52f995832fadd4292ec0121753
Vince Varga <vargavince91@gmail.com>
2019-10-17 13:09:14 +0700

    📖 Add examples: custom output, custom field names

---
+++ example/custom_fields/lib/src/pubspec.dart
@@ -0,0 +1,11 @@
+// Generated file. Do not modify.
+//
+// This file is generated using the build_pubspec package.
+// For more information, go to: https://pub.dev/packages/build_pubspec
+const List<String> betterAuthors = [
+  'Vince Varga <vince.varga@serial.dev>',
+];
+const String betterDescription =
+    '''This is an example description. Great job! Awesome package.''';
+const String betterName = '''build_pubspec_example''';
+const String betterVersion = '''0.1.3''';
